79ef29c79bc04fafd2685b5f69a96135c2a2169e
Deyver Reyes Faria <deyverreyesfaria@gmail.com>
2025-02-07 16:40:03 -0400

    holi

--- deyverFeature.js
+++ deyverFeature.js
@@ -1,3 +1,8 @@
 console.log("Feature 2: Hello from feature branch 2");
+
+console.log("Feature 2: Hello from feature branch 2");
+console.log("Access granted. Welcome.");
+console.log("Access granted retard.");
+
 console.log("Access granted. Welcome.");
 console.log("Access granted retard.");
